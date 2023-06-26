db025a37db921ef175e57dccc11a90e27d0256b4
Tihomir Kit <mail@tkit.dev>
2023-06-26 22:24:54 +0100

    .bashrc installation bugfixes

--- install_bashrc.sh
+++ install_bashrc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 cp ~/my_data/.bashrc.bak ~/.bashrc
-echo "" >> ~/.bashrc
-echo "# Planq additions to .bashrc" >> ~/.bashrc
-echo "source ~/my_data/planq/.bashrc" >> ~/.bashrc
+
+# Append planq .bashrc to .bashrc if it's not already there
+if ! grep -q "planq" filename; then
+  echo "" >> ~/.bashrc
+  echo "# Planq additions to .bashrc" >> ~/.bashrc
+  echo "source ~/my_data/planq/.bashrc" >> ~/.bashrc
+fi
+
 
 # Reload .bashrc
 . ~/.bashrc
