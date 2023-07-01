89416cc082b367cbdfa17594bbc1a060284db3f2
Tihomir Kit <mail@tkit.dev>
2023-07-01 23:43:43 +0100

    Better buster.

--- tools/gobuster.sh
+++ tools/gobuster.sh
@@ -1,9 +1,15 @@
 function bustpages_f {
-  gobuster dir -w /usr/share/dirb/wordlists/common.txt -u $1 -x $2
+  gobuster dir -w /usr/share/dirb/wordlists/common.txt -u $1
+}
+# $1 = url
+alias bustpages=bustpages_f
+
+function bustpageextensions_f {
+  bustpages -u $1 -x $2
 }
 # $1 = url
 # $2 = file extensions (comma-separated)
-alias bustpages=bustpages_f
+alias bustpageextensions=bustpageextensions_f
 
 function bustsubdomains_f {
   gobuster vhost -w /opt/useful/SecLists/Discovery/DNS/subdomains-top1million-5000.txt -u $1
