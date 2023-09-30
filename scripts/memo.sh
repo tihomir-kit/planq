0baf0e18d52544a1eb0bd8eed80c013453b1d394
Tihomir Kit <mail@tkit.dev>
2023-09-30 14:42:18 +0100

    Converted aliases into separate scripts to be executed from path.

--- scripts/memo.sh
+++
@@ -1,2 +0,0 @@
-alias px-memo-stabilize-shell1='script /dev/null -c bash'
-alias px-memo-stabilize-shell2='python3 -c ''import pty;pty.spawn("/bin/bash")'''
--- scripts/nuclei.sh
+++
@@ -1,5 +0,0 @@
-function px-nuclei-spray_f {
-  sudo nuclei -u $1
-}
-# Add "trgt" host IP to /etc/hosts
-alias px-nuclei-spray=px-nuclei-spray_f
